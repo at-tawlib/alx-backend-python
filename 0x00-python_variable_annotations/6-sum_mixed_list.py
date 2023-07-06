d4044f4c1fb483502f2ce1d23f7bd45d7941a6f3
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-06 19:03:29 +0000

    Used Union for different types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from typing import List
 """
 Complex types - mixed list
 """
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int,float]]) -> float:
     """returns sum of mixed list of floats and integers"""
-    return sum(mxd_lst)
+    return float(sum(mxd_lst))

dec1bdd8b41f3da464d564ca29eff4b28736c0ea
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-29 22:32:49 +0000

    Add TestGetJson class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
-Parameterize a unit test
+test for utils.py functions
 """
 import unittest
 from utils import access_nested_map as anp
+from utils import get_json
 from parameterized import parameterized, parameterized_class
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Parameterize a unit test"""
 
     @parameterized.expand([
         [{"a": 1},  ["a"], 1],
@@ -24,6 +27,21 @@ class TestAccessNestedMap(unittest.TestCase):
             ])
     def test_access_nested_map_exception(self, nested_map, path):
         """Using assertRaises to test keyError of the function"""
-        #assertRaises(expected, anp, nested_map, path)
         with self.assertRaises(KeyError):
             anp(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Mock HTTP calls"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    def test_get_json(self, url, expected_result):
+        """test get_json of utils.py"""
+        mock_res = Mock()
+        mock_res.json.return_value = expected_result
+        with patch('requests.get', return_value=mock_res):
+            response = get_json(url)
+            self.assertEqual(response, expected_result)

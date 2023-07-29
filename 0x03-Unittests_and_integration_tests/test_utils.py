bdf0092021958bdf9831312e0a74ed442ec0e5c8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-07-29 18:20:38 +0000

    Add test for exception(KeyError)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,3 +17,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """ testing the method with assert"""
         self.assertEqual(anp(nested_map, path), expected)
+
+    @parameterized.expand([
+            [{}, ["a"]],
+            [{"a": 1}, ["a", "b"]]
+            ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Using assertRaises to test keyError of the function"""
+        #assertRaises(expected, anp, nested_map, path)
+        with self.assertRaises(KeyError):
+            anp(nested_map, path)
